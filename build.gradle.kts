4d3530bc5e159cbb463d9668da271969c6639f23
Brian M. Carr <delphyne@gmail.com>
2025-08-07 17:00:36 -0500

    Initial build scaffolding * Setup projects    * main project for compiler    * sub-project for model    * sub-project for an example that uses the model and compiler * Setup graal native image    * single-arch for now (whatever arch is running) * Minimal plugin code that can record the inputs or replay them * Minimal github workflow with single arch

---
+++ build.gradle.kts
@@ -0,0 +1,180 @@
+import org.gradle.kotlin.dsl.configure
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
+
+plugins {
+    kotlin("jvm")
+
+    id("com.google.osdetector")
+    id("org.graalvm.buildtools.native")
+    application
+
+    // load plugins for downstream, even though they aren't used in the root project
+    // this prevents multiple copies of the plugins from being active, and allows us to configure them here
+    id("com.google.protobuf").apply(false)
+    id("org.jlleitschuh.gradle.ktlint").apply(false)
+}
+
+fun calculateVersion(): String {
+    return System
+        .getenv("ENGINE_BUILD_VERSION")
+        ?.let {
+            it.ifEmpty {
+                null
+            }
+        }
+        ?: "0.0.0-pre.0" // temporary fallback version
+}
+
+description = "protoc compiler to turn gRPC services into openapi v3.1 specs"
+
+dependencies {
+    implementation(projects.protocGenOpenapiModel)
+
+    implementation(libs.protobuf.java)
+}
+
+allprojects {
+    apply<IdeaPlugin>()
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    group = "com.engine"
+    version = calculateVersion()
+
+    repositories {
+        mavenCentral()
+    }
+
+    configure<JavaPluginExtension> {
+        withJavadocJar()
+        withSourcesJar()
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of("21"))
+            vendor.set(JvmVendorSpec.GRAAL_VM)
+        }
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    configure<KotlinJvmProjectExtension> {
+        explicitApi()
+    }
+
+    configure<KtlintExtension> {
+        version.set("1.5.0")
+        filter {
+            /*
+             * work around bug in the ktlint plugin that doesn't honor exclusions of
+             * generated code (protobuf, etc.)
+             */
+            exclude {
+                it.file.absolutePath.startsWith(layout.buildDirectory.get().asFile.absolutePath)
+            }
+        }
+        reporters {
+            reporter(ReporterType.CHECKSTYLE)
+            reporter(ReporterType.HTML)
+        }
+    }
+
+    tasks.withType<Jar>().configureEach {
+        manifest {
+            attributes(
+                "Name" to project.name,
+                "Specification-Title" to rootProject.name,
+                "Specification-Version" to version,
+                "Specification-Vendor" to "HotelEngine, Inc., d/b/a Engine",
+            )
+        }
+    }
+
+    afterEvaluate {
+        configure<TestingExtension> {
+            suites {
+                configureEach {
+                    if (this is JvmTestSuite) {
+                        useJUnitJupiter()
+                        dependencies {
+                            implementation.bundle(testLibs.bundles.kotest)
+                        }
+                    }
+                }
+            }
+        }
+
+        tasks.withType<Test>().configureEach {
+            jvmArgs("--add-opens=java.base/java.util=ALL-UNNAMED")
+        }
+    }
+
+    configure<KtlintExtension> {
+        version.set("1.7.1")
+        filter {
+            /*
+             * work around bug in the ktlint plugin that doesn't honor exclusions of
+             * generated code (protobuf, etc.)
+             */
+            exclude {
+                it.file.absolutePath.startsWith(layout.buildDirectory.get().asFile.absolutePath)
+            }
+        }
+        reporters {
+            reporter(ReporterType.CHECKSTYLE)
+            reporter(ReporterType.HTML)
+        }
+    }
+}
+
+application {
+    mainClass.set("com.engine.protoc.openapi.MainKt")
+}
+
+graalvmNative {
+    toolchainDetection = false
+    binaries {
+        named("main") {
+            imageName = "${project.name}-${osdetector.arch}"
+            mainClass = "com.engine.protoc.openapi.MainKt"
+            sharedLibrary = false
+            verbose = true
+            resources.autodetect()
+            fallback = false
+        }
+        all {
+            verbose = true
+            javaLauncher.set(javaToolchains.launcherFor {
+                languageVersion.set(JavaLanguageVersion.of(21))
+                vendor.set(JvmVendorSpec.GRAAL_VM)
+            })
+            buildArgs.add("-H:ThrowMissingRegistrationErrors=")
+        }
+    }
+    agent {
+        enabled = true
+        metadataCopy {
+            inputTaskNames.add("run")
+            outputDirectories.add("src/main/resources/META-INF/native-image/com.engine/protoc-gen-openapi")
+            mergeWithExisting = true
+        }
+    }
+    metadataRepository {
+        enabled = true
+        version = "0.3.24"
+    }
+}
+
+val writeVersion = tasks.register("writeVersion") {
+    val versionFile = project.layout.buildDirectory.map { it.file("version.txt") }
+    group = "build"
+    outputs.file(versionFile)
+    outputs.upToDateWhen { !versionFile.get().asFile.exists() || versionFile.get().asFile.readText() != version.toString() }
+    doFirst {
+        versionFile
+            .get()
+            .asFile
+            .apply { parentFile.mkdirs() }
+            .writeText(version.toString())
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,73 @@
+rootProject.name = "protoc-gen-openapi"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+    }
+    plugins {
+        val kotlinVersion: String by settings // this comes from gradle.properties
+        kotlin("jvm").version(kotlinVersion)
+
+        id("com.google.osdetector").version("1.7.3")
+        id("com.google.protobuf").version("0.9.4")
+        id("org.graalvm.buildtools.native").version("0.11.0")
+        id("org.jlleitschuh.gradle.ktlint").version("13.0.0")
+    }
+}
+
+dependencyResolutionManagement {
+    val protobuf = "4.31.1"
+
+    versionCatalogs {
+        create("libs") {
+            val protobuf = version("protobuf", protobuf)
+
+            library("protobuf.java", "com.google.protobuf", "protobuf-java").versionRef(protobuf)
+        }
+        create("testLibs") {
+            val kotest = version("kotest", "5.9.1")
+
+            val krj5 =
+                "kotest.runner.junit5".apply {
+                    library(
+                        "kotest.runner.junit5",
+                        "io.kotest",
+                        "kotest-runner-junit5",
+                    ).versionRef(kotest)
+                }
+            val kac =
+                "kotest.assertions.core".apply {
+                    library(
+                        "kotest.assertions.core",
+                        "io.kotest",
+                        "kotest-assertions-core",
+                    ).versionRef(kotest)
+                }
+
+            bundle("kotest", listOf(krj5, kac))
+        }
+        create("tools") {
+            val protobuf = version("protobuf", protobuf)
+
+            library("protoc.compiler", "com.google.protobuf", "protoc").versionRef(protobuf)
+            library("protoc.gen.doc", "io.github.pseudomuto", "protoc-gen-doc").version("1.5.1")
+        }
+    }
+}
+
+rootDir
+    .walkTopDown()
+    .filter { it != rootDir }
+    .filter { it.isDirectory }
+    .filterNot { it.name.startsWith(".") }
+    .filterNot { setOf("build", "buildSrc", "tmp", "scratch").contains(it.name) }
+    .filterNot { it.resolve(".gradle_ignore").exists() }
+    .filter { it.resolve("build.gradle.kts").let { it.exists() && it.isFile } }
+    .forEach {
+        val relativePath = it.relativeTo(rootDir)
+        val projectName = ":${rootProject.name}-${relativePath.path.replace("/", "-")}"
+        include(projectName)
+        project(projectName).projectDir = it
+    }
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")

ca81264ed236f5559e141428d404d0c7b36b3324
Brian M. Carr <delphyne@gmail.com>
2025-08-11 09:32:21 -0500

    Initial build scaffolding (#1)
    * Setup projects
       * main project for compiler
       * sub-project for model
       * sub-project for an example that uses the model and compiler
    * Setup graal native image
       * single-arch for now (whatever arch is running)
    * Minimal plugin code that can record the inputs or replay them
    * Minimal github workflow with single arch

---
+++ example/build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+
+plugins {
+    id("com.google.protobuf")
+}
+
+description = "An example project that uses the protoc-gen-openpi plugin"
+
+dependencies {
+    api(libs.protobuf.java)
+    implementation(projects.protocGenOpenapiModel)
+}
+
+kotlin {
+    explicitApi = ExplicitApiMode.Disabled
+}
+
+protobuf {
+    protoc {
+        artifact = tools.protoc.compiler.get().toString()
+    }
+    plugins {
+        create("openapi") {
+            path = rootProject
+                .layout
+                .buildDirectory
+                .map { it.dir("native/nativeCompile").file("${rootProject.name}-${osdetector.arch}") }
+                .get()
+                .asFile
+                .absolutePath
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.dependsOn(
+                project
+                    .project(projects.protocGenOpenapi.path)
+                    .tasks
+                    .first { it.name == "nativeCompile" }
+            )
+            it.plugins {
+                create("openapi") {
+                    option("recordCodeGenertorRequest=/var/tmp/protoc-gen-openapi.cgreq")
+                    option("recordCodeGeneratorResponse=/var/tmp/protoc-gen-openapi.cgresp")
+                }
+            }
+        }
+    }
+}
---
+++ model/build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
+
+plugins {
+    id("com.google.protobuf")
+}
+
+description = "OpenAPI annotations for protobuf Messages, Enums, Services, and RPCs"
+
+dependencies {
+    api(libs.protobuf.java)
+}
+
+kotlin {
+    explicitApi = ExplicitApiMode.Disabled
+}
+
+protobuf {
+    protoc {
+        artifact = tools.protoc.compiler.get().toString()
+    }
+    plugins {
+        create("doc") {
+            artifact = tools.protoc.gen.doc.get().toString()
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.generateDescriptorSet = true
+            it.descriptorSetOptions.includeImports = true
+
+            it.plugins {
+                create("doc") {
+                    option("markdown,${project.name}-${version}.md")
+                }
+            }
+        }
+    }
+}

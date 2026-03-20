1aec5f02be182a3c3f830a71d4c5e25de8fe272e
Brian M. Carr <brian.carr@engine.com>
2026-03-20 16:47:47 -0500

    different attempt to disable example project for codeql

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -18,36 +18,45 @@ kotlin {
 // TODO:
 //  disabled until issues with codeql and native compile are addressed.
 //  this should probably be a testResource and not a
-if (!project.hasProperty("codeql")) {
-    protobuf {
-        protoc {
-            artifact = tools.protoc.compiler.get().toString()
+protobuf {
+    protoc {
+        artifact = tools.protoc.compiler.get().toString()
+    }
+    plugins {
+        create("openapi") {
+            path = rootProject
+                .layout
+                .buildDirectory
+                .map { it.dir("native/nativeCompile").file("${rootProject.name}-${osdetector.arch}") }
+                .get()
+                .asFile
+                .absolutePath
         }
-        plugins {
-            create("openapi") {
-                path = rootProject
-                    .layout
-                    .buildDirectory
-                    .map { it.dir("native/nativeCompile").file("${rootProject.name}-${osdetector.arch}") }
-                    .get()
-                    .asFile
-                    .absolutePath
+    }
+    generateProtoTasks {
+        all().all {
+            dependsOn(
+                project
+                    .project(projects.protocGenOpenapi.path)
+                    .tasks
+                    .named("nativeCompile")
+            )
+            plugins {
+                create("openapi") {
+                    option("recordCodeGeneratorRequest=/var/tmp/protoc-gen-openapi.cgreq")
+                    option("recordCodeGeneratorResponse=/var/tmp/protoc-gen-openapi.cgresp")
+                }
             }
         }
-        generateProtoTasks {
-            all().all {
-                dependsOn(
-                    project
-                        .project(projects.protocGenOpenapi.path)
-                        .tasks
-                        .named("nativeCompile")
-                )
-                plugins {
-                    create("openapi") {
-                        option("recordCodeGeneratorRequest=/var/tmp/protoc-gen-openapi.cgreq")
-                        option("recordCodeGeneratorResponse=/var/tmp/protoc-gen-openapi.cgresp")
-                    }
-                }
+    }
+}
+
+if (!project.hasProperty("codeql")) {
+    gradle.taskGraph.whenReady {
+        gradle.taskGraph.allTasks.forEach {
+            if (it.project == project) {
+                logger.quiet("Disabling ${it.name} because codeql is running")
+                it.enabled = false
             }
         }
     }

dea94a7dc54cd8a1986961e753bbe0cdd5b58a4b
Brian M. Carr <brian.carr@engine.com>
2026-03-20 16:39:23 -0500

    disable example proto when in codeql

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -15,33 +15,38 @@ kotlin {
     explicitApi = ExplicitApiMode.Disabled
 }
 
-protobuf {
-    protoc {
-        artifact = tools.protoc.compiler.get().toString()
-    }
-    plugins {
-        create("openapi") {
-            path = rootProject
-                .layout
-                .buildDirectory
-                .map { it.dir("native/nativeCompile").file("${rootProject.name}-${osdetector.arch}") }
-                .get()
-                .asFile
-                .absolutePath
+// TODO:
+//  disabled until issues with codeql and native compile are addressed.
+//  this should probably be a testResource and not a
+if (!project.hasProperty("codeql")) {
+    protobuf {
+        protoc {
+            artifact = tools.protoc.compiler.get().toString()
         }
-    }
-    generateProtoTasks {
-        all().all {
-            dependsOn(
-                project
-                    .project(projects.protocGenOpenapi.path)
-                    .tasks
-                    .named("nativeCompile")
-            )
-            plugins {
-                create("openapi") {
-                    option("recordCodeGeneratorRequest=/var/tmp/protoc-gen-openapi.cgreq")
-                    option("recordCodeGeneratorResponse=/var/tmp/protoc-gen-openapi.cgresp")
+        plugins {
+            create("openapi") {
+                path = rootProject
+                    .layout
+                    .buildDirectory
+                    .map { it.dir("native/nativeCompile").file("${rootProject.name}-${osdetector.arch}") }
+                    .get()
+                    .asFile
+                    .absolutePath
+            }
+        }
+        generateProtoTasks {
+            all().all {
+                dependsOn(
+                    project
+                        .project(projects.protocGenOpenapi.path)
+                        .tasks
+                        .named("nativeCompile")
+                )
+                plugins {
+                    create("openapi") {
+                        option("recordCodeGeneratorRequest=/var/tmp/protoc-gen-openapi.cgreq")
+                        option("recordCodeGeneratorResponse=/var/tmp/protoc-gen-openapi.cgresp")
+                    }
                 }
             }
         }
